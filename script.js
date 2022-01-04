1d3299c7eed9cd687202797d80d3cddbcb864e79
Sofie Bierre Hansen <sofi02a7@stud.kea.dk>
2022-01-04 22:31:12 +0100

    tirsdag sidste commit

--- script.js
+++ script.js
@@ -17,3 +17,26 @@ function toggleMenu() {
     document.querySelector("#menuknap").textContent = "X";
   }
 }
+
+// Dannies burger
+
+// const btn = document.querySelector("button");
+// const ul = document.querySelector("nav");
+
+// function toggleMenu() {
+//   ul.classList.toggle("shown");
+
+//   const menu = ul.classList.contains("shown");
+
+//   if (menu) {
+//     // hvis ul har klassen "shown"
+//     //btn.textContent = "Luk";
+//     btn.classList.add("open");
+//   } else {
+//     // hvis IKKE ul har klassen "shown"
+//     //btn.textContent = "Menu";
+//     btn.classList.remove("open");
+//   }
+// }
+
+// btn.addEventListener("click", toggleMenu);
